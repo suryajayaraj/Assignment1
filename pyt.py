e647fc1017e43352d6436e779f01882d3ec46a7e
suryajayaraj <43784796+suryajayaraj@users.noreply.github.com>
2025-04-18 16:16:35 +0530

    Add files via upload

---
+++ pyt.py
@@ -0,0 +1,10 @@
+x=float(input('Enter first number:'))
+y=float(input('Enter the second number:'))
+Add = x+y
+sub = x - y
+div = x/y
+mul = x*y
+print('Addition of two number is:', Add)
+print('subtraction of two number is:',sub)
+print('division of two number is :',div)
+print('multiplication of two numbers is:',mul)
